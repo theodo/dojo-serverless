5f44a51f48220d8610153f1e01c550ff988bc0fb
CorentinDoue <corentin.doue.pro@gmail.com>
2020-07-26 23:59:52 +0200

    feat: add virus on websocket message

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -11,6 +11,8 @@ import virus4 from 'assets/Virus4.png';
 import virus5 from 'assets/Virus5.png';
 import virus6 from 'assets/Virus6.png';
 
+import { websocketConnexion } from '../services/networking/websocket';
+
 const VirusImgs = [virus1, virus2, virus3, virus4, virus5, virus6];
 
 const { Title, Text } = Typography;
@@ -84,6 +86,14 @@ export default () => {
       method: 'DELETE',
     });
     setViruses((prevViruses) => prevViruses.filter(({ id }) => id !== virusId));
+  };
+
+  websocketConnexion.onmessage = (message) => {
+    const data = JSON.parse(message.data);
+    const virusId = data.virusId;
+    if (virusId) {
+      setViruses((prevViruses) => prevViruses.concat(getRandomVirus(virusId)));
+    }
   };
 
   return (
