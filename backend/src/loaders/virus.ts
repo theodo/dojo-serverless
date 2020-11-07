2b19a9a3511e9bfb85fa24e948b60b6e7efdb4a0
Matthieu Auger <matthieua@theodo.fr>
2020-11-07 17:25:07 +0100

    Move logic outside handler

---
+++ backend/src/loaders/virus.ts
@@ -0,0 +1,22 @@
+export const fetchViruses: any = () => {
+  return [
+    {
+      id: '9d6fa82f-2edc-43d6-a7a6-7ac4a6f0dcca',
+      positionX: 30,
+      positionY: 24,
+      src: '/static/media/Virus1.d02ce17d.png',
+    },
+    {
+      id: 'd8408a56-ecd6-464b-9616-c5a456bfb4f8',
+      positionX: 39,
+      positionY: 74,
+      src: '/static/media/Virus1.d02ce17d.png',
+    },
+    {
+      id: '4b03e963-1bcf-4054-9b0c-cb866cfb7f23',
+      positionX: 11,
+      positionY: 94,
+      src: '/static/media/Virus6.9a59198b.png',
+    },
+  ];
+};
