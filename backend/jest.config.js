e85eaf34c7a3dd20ff1d6b6cf3591125ef450d68
Matthieu Auger <matthieua@theodo.fr>
2020-11-07 18:28:38 +0100

    Add some tests yay

---
+++ backend/jest.config.js
@@ -0,0 +1,14 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+// In the following statement, replace `./tsconfig` with the path to your `tsconfig` file
+// which contains the path mapping (ie the `compilerOptions.paths` option):
+const { compilerOptions } = require('./tsconfig.paths');
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: 'src/',
+  }),
+  modulePaths: ['src/'],
+  moduleDirectories: ['node_modules'],
+};
