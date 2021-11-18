4c1e8963bdc0c6725e610e4c77ec259f529d0242
Maxime Vivier <maxime.vivier@centrale-marseille.fr>
2021-11-18 11:36:35 +0100

    verif faite par api gateway

--- backend/src/functions/virus-kill/handler.ts
+++ backend/src/functions/virus-kill/handler.ts
@@ -1,12 +1,8 @@
-import { success, failure } from '@libs/apiGateway';
+import { success } from '@libs/apiGateway';
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import { middyfy } from '@libs/lambda';
 
 const kill: APIGatewayProxyHandler = async (event) => {
-  if (!event.pathParameters || !event.pathParameters.id) {
-    return failure({ message: 'No id provided' });
-  }
-
   const { id } = event.pathParameters;
 
   console.log('Virus killed');
