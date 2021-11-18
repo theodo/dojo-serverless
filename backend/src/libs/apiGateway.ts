8d6dd8ec727ff61e1dcdbf348ff69b5823d55462
Maxime Vivier <maxime.vivier@centrale-marseille.fr>
2021-11-18 12:36:46 +0100

    add cors headers

--- backend/src/libs/apiGateway.ts
+++ backend/src/libs/apiGateway.ts
@@ -2,11 +2,15 @@ import type { APIGatewayProxyEvent, APIGatewayProxyResult, Handler } from "aws-l
 import type { FromSchema } from "json-schema-to-ts";
 
 type ValidatedAPIGatewayProxyEvent<S> = Omit<APIGatewayProxyEvent, 'body'> & { body: FromSchema<S> }
-export type ValidatedEventAPIGatewayProxyEvent<S> = Handler<ValidatedAPIGatewayProxyEvent<S>, APIGatewayProxyResult>
+export type ValidatedEventAPIGatewayProxyHandler<S> = Handler<ValidatedAPIGatewayProxyEvent<S>, APIGatewayProxyResult>
 
 export const formatJSONResponse = (response: Record<string, unknown>) => {
   return {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': 'http://localhost:3000',
+      'Access-Control-Allow-Credentials': 'true',
+    },
     body: JSON.stringify(response)
   }
 }
