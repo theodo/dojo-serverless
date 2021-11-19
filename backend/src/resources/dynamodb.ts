a2ff16dbe269c8a2ba49004e6e4ac35cb74a372c
Maxime Vivier <maxime.vivier@centrale-marseille.fr>
2021-11-19 12:12:02 +0100

    uncomment whole file because useless to comment it

--- backend/src/resources/dynamodb.ts
+++ backend/src/resources/dynamodb.ts
@@ -1,12 +1,12 @@
-// import { Table, AttributeType, StreamViewType, BillingMode } from '@aws-cdk/aws-dynamodb';
-// import { Construct } from '@aws-cdk/core';
+import { Table, AttributeType, StreamViewType, BillingMode } from '@aws-cdk/aws-dynamodb';
+import { Construct } from '@aws-cdk/core';
 
-// export default (stack: Construct) => {
-//   return new Table(stack, 'DojoServerlessTable', {
-//     tableName: "dojo-serverless-table",
-//     billingMode: BillingMode.PAY_PER_REQUEST,
-//     stream: StreamViewType.NEW_AND_OLD_IMAGES,
-//     partitionKey: { name: 'partitionKey', type: AttributeType.STRING },
-//     sortKey: { name: 'sortKey', type: AttributeType.STRING },
-//   });
-// }
+export default (stack: Construct) => {
+  return new Table(stack, 'DojoServerlessTable', {
+    tableName: "dojo-serverless-table",
+    billingMode: BillingMode.PAY_PER_REQUEST,
+    stream: StreamViewType.NEW_AND_OLD_IMAGES,
+    partitionKey: { name: 'partitionKey', type: AttributeType.STRING },
+    sortKey: { name: 'sortKey', type: AttributeType.STRING },
+  });
+}
