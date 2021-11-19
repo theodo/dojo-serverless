ec86d7f2e2e237a002d33bcfa6e064515add1e8f
Maxime Vivier <maxime.vivier@centrale-marseille.fr>
2021-11-19 13:37:16 +0100

    implement dynamodb on each route

--- backend/src/functions/virus-create/handler.ts
+++ backend/src/functions/virus-create/handler.ts
@@ -2,11 +2,25 @@ import { formatJSONResponse } from '@libs/apiGateway';
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import { middyfyWithoutBodyParser } from '@libs/lambda';
 import uuid from 'uuid';
+import {
+  PutCommand,
+  DynamoDBDocumentClient
+} from '@aws-sdk/lib-dynamodb';
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 
-const create: APIGatewayProxyHandler = async () => {
+const documentClient = DynamoDBDocumentClient.from(new DynamoDBClient({}));
+
+const kill: APIGatewayProxyHandler = async () => {
   const id = uuid();
-  console.log('Virus created');
+
+  await documentClient.send(
+    new PutCommand({
+      TableName: 'dojo-serverless-table',
+      Item: { partitionKey: 'Virus', sortKey: id },
+    }),
+  );
+
   return formatJSONResponse({ id });
 }
 
-export const main = middyfyWithoutBodyParser(create);
+export const main = middyfyWithoutBodyParser(kill);
--- backend/src/functions/virus-get/handler.ts
+++ backend/src/functions/virus-get/handler.ts
@@ -1,16 +1,24 @@
 import { formatJSONResponse } from '@libs/apiGateway';
+import { Virus } from '../types';
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import { middyfyWithoutBodyParser } from '@libs/lambda';
-import uuid from 'uuid';
+import {
+  QueryCommand,
+  DynamoDBDocumentClient
+} from '@aws-sdk/lib-dynamodb';
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+
+const documentClient = DynamoDBDocumentClient.from(new DynamoDBClient({}));
 
 const get: APIGatewayProxyHandler = async () => {
-  const viruses = [
-    { id: uuid() },
-    { id: uuid() },
-    { id: uuid() },
-    { id: uuid() },
-  ];
-  return formatJSONResponse({ viruses });
+  const { Items = [] } = await documentClient.send(
+    new QueryCommand({
+      TableName: 'dojo-serverless-table',
+      KeyConditionExpression: 'partitionKey = :partitionKey',
+      ExpressionAttributeValues: { ':partitionKey': 'Virus' },
+    }),
+  );
+  return formatJSONResponse({ viruses: (Items as Virus[]).map(({ sortKey }) => ({ id: sortKey })) });
 }
 
 export const main = middyfyWithoutBodyParser(get);
