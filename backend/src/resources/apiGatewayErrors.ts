f6ebee771b223ea22c57913ef7474c9c5674383e
Maxime Vivier <maxime.vivier@centrale-marseille.fr>
2021-11-19 11:51:45 +0100

    split resources and initialisation comment incomming changes

--- backend/src/resources/apiGatewayErrors.ts
+++ backend/src/resources/apiGatewayErrors.ts
@@ -1,23 +1,22 @@
-import { App, Fn, Stack } from '@aws-cdk/core';
+import { Fn, Construct } from '@aws-cdk/core';
 import { CfnGatewayResponse } from '@aws-cdk/aws-apigateway';
 
-export const app = new App();
-export const stack = new Stack(app, 'Stack');
+export default (stack: Construct) => {
+  new CfnGatewayResponse(stack, 'GatewayResponseDefault4XX', {
+    responseType: 'DEFAULT_4XX',
+    restApiId: Fn.ref('ApiGatewayRestApi'),
+    responseParameters: {
+      'gatewayresponse.header.Access-Control-Allow-Origin': "'*'",
+      'gatewayresponse.header.Access-Control-Allow-Headers': "'*'",
+    },
+  });
 
-new CfnGatewayResponse(stack, 'GatewayResponseDefault4XX', {
-  responseType: 'DEFAULT_4XX',
-  restApiId: Fn.ref('ApiGatewayRestApi'),
-  responseParameters: {
-    'gatewayresponse.header.Access-Control-Allow-Origin': "'*'",
-    'gatewayresponse.header.Access-Control-Allow-Headers': "'*'",
-  },
-});
-
-new CfnGatewayResponse(stack, 'GatewayResponseDefault5XX', {
-  responseType: 'DEFAULT_5XX',
-  restApiId: Fn.ref('ApiGatewayRestApi'),
-  responseParameters: {
-    'gatewayresponse.header.Access-Control-Allow-Origin': "'*'",
-    'gatewayresponse.header.Access-Control-Allow-Headers': "'*'",
-  },
-});
+  new CfnGatewayResponse(stack, 'GatewayResponseDefault5XX', {
+    responseType: 'DEFAULT_5XX',
+    restApiId: Fn.ref('ApiGatewayRestApi'),
+    responseParameters: {
+      'gatewayresponse.header.Access-Control-Allow-Origin': "'*'",
+      'gatewayresponse.header.Access-Control-Allow-Headers': "'*'",
+    },
+  });
+}
